c49f0dba84222cdfa54e92984d75fa0a323b6597
prashanth kumar <32808287+pacchi1997@users.noreply.github.com>
2021-08-31 16:59:23 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
+import React,{useState} from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
+function App() {
+  const [cartShow,setCartShow]= useState(false);
+ 
+ const showCartHandler=()=>{
+   setCartShow(true);
+ }
+
+ const hideCartHandler=()=>{
+   setCartShow(false);
+ }
+  return (
+    <CartProvider>
+     { cartShow && <Cart onHideCart={hideCartHandler}/> }
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+      </CartProvider>
+  );
+}
+
+export default App;
